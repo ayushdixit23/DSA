12aa51cf3c928727a03ab6ad6d95135fbad769b7
Ayush Dixit <fsayush100@gmail.com>
2025-03-26 17:10:46 +0530

    completed array methods

--- arrays/basics.js
+++ arrays/basics.js
@@ -1,17 +1,13 @@
-const iterateInArrays = (arr) => {
+export const iterateInArraysAndConsoleLogs = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     console.log(arr[i], `is at ${i} index`);
   }
 };
 
-const arr = [1, 2, 3, 4];
-// iterateInArrays(arr);
+// const arr = [1, 2, 3, 4];
+// iterateInArraysAndConsoleLogs(arr);
 
 // common array methods
-
-// arr.forEach((value, index, array) => {
-//   console.log(value, index, array);
-// });
 
 // const fruits = ["Banana", "Orange", "Apple"];
 // let type = typeof fruits;
@@ -21,12 +17,10 @@ const arr = [1, 2, 3, 4];
 
 // const fruits = ["Banana", "Orange", "Apple", "Mango"];
 // const stringifyFruits = fruits.toString(); // array converted to string
-
 // console.log(stringifyFruits,"stringifyFruits") //string
 
 // const fruits = ["Banana", "Orange", "Apple", "Mango"];
 // const joinFruits =  fruits.join(" * "); //array converted to string with joining  *
-
 // console.log(joinFruits,"joinFruits") // "Banana * Orange * Apple * Mango"
 
 // shift() => removes first elements of array
@@ -108,3 +102,72 @@ const arr = [1, 2, 3, 4];
 // const temp = [27, 28, 30, 40, 42, 35, 30];
 // let pos = temp.findLastIndex(x => x > 40);
 // console.log(pos, "pos"); //4
+
+// Sorting Methods
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.sort();
+// console.log(fruits, "fruits"); // [Apple,Banana,Mango,Orange]
+
+// const arr = [5, 2, 9, 1, 5, 6];
+
+// arr.sort((a, b) => a - b); // Ascending
+// console.log(arr); // [1, 2, 5, 5, 6, 9]
+
+// a-b => +ve means swap meaning  a is swaped by b -> a <=> b
+// a-b => -ve means no swap
+
+// arr.sort((a, b) => b - a); // Descending
+// console.log(arr); // [9, 6, 5, 5, 2, 1]
+
+// toSorted() method as a safe way to sort an array without altering the original array.
+
+// const months = ["Jan", "Feb", "Mar", "Apr"];
+// const sorted = months.toSorted();
+// console.log(sorted,"sorted") ["Apr","Feb","Jan","Mar"]
+
+// iteration methods
+
+// arr.forEach((value, index, array) => {
+//   console.log(value, index, array);
+// });
+
+// const numbers = [45, 4, 9, 16, 25];
+// const numberGreaterThan18 = numbers.filter((value)=> {
+//     return value > 18
+// })
+
+// console.log(numberGreaterThan18,"numberGreaterThan18")
+
+// The reduce() method runs a function on each array element to produce (reduce it to) a single value
+
+// const numbers = [1, 2, 3, 4, 5];
+// let sum = numbers.reduce((acc, value) => {
+//   return acc + value;
+// });
+
+// console.log(sum, "sum");
+
+// The every() method checks if all array values pass a test.
+// const numbers = [45, 4, 9, 16, 25];
+// let allOver18 = numbers.every(myFunction);
+
+// function myFunction(value, index, array) {
+//   return value > 18;
+// }
+// output => false
+
+
+// The some() method checks if some array values pass a test.
+
+// const numbers = [45, 4, 9, 16, 25];
+// let someOver18 = numbers.some(myFunction);
+
+// function myFunction(value, index, array) {
+//   return value > 18;
+// }
+
+// Output => true
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const keys = fruits.keys();
+// console.log(keys,"keys")
