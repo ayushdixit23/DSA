a6e4e0526f696a2a5a8408d749998e59cf2542b2
Ayush Dixit <fsayush100@gmail.com>
2025-03-27 15:33:05 +0530

    quick sort and merge sort added

--- arrays/sorting.js
+++ arrays/sorting.js
@@ -93,7 +93,7 @@ const countingSort = () => {
     const frequency = object[num];
 
     for (let i = 0; i < frequency; i++) {
-      myArr[index++] = Number(num); 
+      myArr[index++] = Number(num);
     }
   }
 };
@@ -101,3 +101,54 @@ const countingSort = () => {
 // countingSort();
 // console.log(myArr);
 
+// Quick Sort
+const quickSort = (arr) => {
+  if (arr.length <= 1) return arr;
+
+  const pivot = arr[arr.length - 1];
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
+};
+
+// console.log(quickSort(arr));
+
+function merge(left, right) {
+  let sortedArray = [];
+  let i = 0,
+    j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      sortedArray.push(left[i]);
+      i++;
+    } else {
+      sortedArray.push(right[j]);
+      j++;
+    }
+  }
+
+  return [...sortedArray, ...left.slice(i), ...right.slice(j)];
+}
+
+// Merge Sort
+const mergeSort = (arr) => {
+  if (arr.length <= 1) return arr;
+
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
+};
+
+// console.log(mergeSort(arr));
