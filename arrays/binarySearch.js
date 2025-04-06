1d596e2ed9364c4b75ee66f4dc8d4ebb142d1d38
Ayush Dixit <fsayush100@gmail.com>
2025-04-06 17:32:10 +0530

    searching in 2d matrix

--- arrays/binarySearch.js
+++ arrays/binarySearch.js
@@ -47,15 +47,103 @@ const search = function () {
 // console.log(search());
 
 // Find Peak Element
-const findPeakElement = function () {
-  let start = 1;
-  let end = nums.length - 2;
+// const findPeakElement = function () {
+//   let start = 1;
+//   let end = nums.length - 2;
 
-  while (start <= end) {
-    
-  }
-};
+//   while (start <= end) {}
+// };
 
 const nums = [1, 2, 3, 1];
 
-console.log(findPeakElement());
+// console.log(findPeakElement());
+
+function floorSqrt(n) {
+  const answer = Math.floor(Math.sqrt(n));
+  return answer;
+}
+
+function nthRoot(n, m) {
+  let start = 1;
+  let end = m;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (Math.pow(mid, n) === m) {
+      return mid;
+    } else if (Math.pow(mid, n) > m) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return -1;
+}
+
+// Koko Eating Bananas
+const piles = [3, 6, 7, 11],
+  h = 8;
+
+const minEatingSpeed = function () {
+  let minimumSpeed = 1;
+  let maximumSpeed = -Infinity;
+
+  for (let i = 0; i < piles.length; i++) {
+    maximumSpeed = Math.max(maximumSpeed, piles[i]);
+  }
+
+  while (minimumSpeed < maximumSpeed) {
+    let mid = Math.floor((minimumSpeed + maximumSpeed) / 2);
+
+    let sum = 0;
+    for (let i = 0; i < piles.length; i++) {
+      sum += Math.ceil(piles[i] / mid);
+    }
+
+    if (sum > h) {
+      minimumSpeed = mid + 1;
+    } else {
+      maximumSpeed = mid;
+    }
+  }
+
+  return minimumSpeed;
+};
+
+// console.log(minEatingSpeed());
+
+const searchMatrix = function () {
+  for (let i = 0; i < matrix.length; i++) {
+    const rowArr = matrix[i];
+    if (rowArr[0] <= target && target <= rowArr[rowArr.length - 1]) {
+      let start = 0;
+      let end = rowArr.length - 1;
+      while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+        if (rowArr[mid] === target) {
+          return true;
+        } else if (rowArr[mid] > target) {
+          end = mid - 1;
+        } else {
+          start = mid + 1;
+        }
+      }
+    } else {
+      continue;
+    }
+  }
+  return false;
+};
+
+// const matrix = [
+//     [1, 3, 5, 7],
+//     [10, 11, 16, 20],
+//     [23, 30, 34, 60],
+//   ],
+//   target = 3;
+
+const matrix = [[-10], [-7], [-5]];
+const target = -10;
+
+console.log(searchMatrix());
