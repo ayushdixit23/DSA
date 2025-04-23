ba90b6c9bd8a2fa50a41d95ffa90e9d768a66da9
Ayush Dixit <fsayush100@gmail.com>
2025-04-23 11:09:20 +0530

    questions on subarray

--- arrays/prefixsum.js
+++ arrays/prefixsum.js
@@ -16,7 +16,54 @@ function sumOfSubarraysOfSizeK(arr, k) {
   return sumArr;
 }
 
-const arr = [1, 2, 3, 4, 5];
-const k = 1;
+// const arr = [1, 2, 3, 4, 5];
+// const k = 1;
 // console.log(sumOfSubarraysOfSizeK(arr, k));
 
+// const arr = [1, 2, 3, -2, 2];
+// const k = 3;
+
+// Subarray Sum Equals K
+function subarraySumEqualsK(arr, k) {
+  let count = 0;
+  const map = new Map();
+  let prefixSum = 0;
+  map.set(0, 1);
+  for (let i = 0; i < arr.length; i++) {
+    prefixSum += arr[i];
+
+    const target = prefixSum - k;
+    if (map.has(target)) {
+      count += map.get(target);
+    }
+
+    map.set(prefixSum, (map.get(prefixSum) || 0) + 1);
+  }
+  return count;
+}
+
+// console.log(subarraySumEqualsK(arr, k));
+
+const findMaxAverage = function () {
+  if (k === 1) {
+    return nums[0];
+  }
+  let maxAverage = -Infinity;
+  let sum = nums[0];
+
+  for (let i = 1; i < k; i++) {
+    sum += nums[i];
+  }
+  maxAverage = Math.max(sum / k, maxAverage);
+
+  for (let i = k; i < nums.length; i++) {
+    sum = sum - nums[i - k] + nums[i];
+    maxAverage = Math.max(sum / k, maxAverage);
+  }
+  return maxAverage;
+};
+
+const nums = [1, 12, -5, -6, 50, 3];
+const k = 4;
+
+console.log(findMaxAverage());
