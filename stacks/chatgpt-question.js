30dbc6cacb1291f9afc893f5b4577464a10167c8
Ayush Dixit <fsayush100@gmail.com>
2025-04-24 13:11:48 +0530

    valid parenthesis solved

---
+++ stacks/chatgpt-question.js
@@ -0,0 +1,35 @@
+import { Stack } from "./stack.js";
+
+const isValid = function () {
+  if (s.length === 1) {
+    return false;
+  }
+  const stack = new Stack();
+  for (let i = 0; i < s.length; i++) {
+    const element = s[i];
+    if (element === "(" || element === "[" || element === "{") {
+      stack.push(element);
+    }
+    if (element === "}" || element === "]" || element === ")") {
+      if (stack.isEmpty()) {
+        return false;
+      } else {
+        const peek = stack.peek();
+        if (
+          (element === ")" && peek === "(") ||
+          (element === "]" && peek === "[") ||
+          (element === "}" && peek === "{")
+        ) {
+          stack.pop();
+        }else{
+            return false
+        }
+      }
+    }
+  }
+  return stack.isEmpty() ? true : false
+};
+
+const s = "((";
+
+console.log(isValid())
