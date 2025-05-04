1a8518662f08a1995d79b5af29f68f5de13e2bdb
Ayush Dixit <fsayush100@gmail.com>
2025-05-04 15:50:03 +0530

    largest area in histogram

--- stacks/leetcode.js
+++ stacks/leetcode.js
@@ -1,3 +1,5 @@
+import { Stack } from "./stack.js";
+
 const calculate = function (s) {
   const numberStack = new Stack();
   const operatorStack = new Stack();
@@ -83,5 +85,94 @@ const trap = function () {
   return total;
 };
 
-const height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
-console.log(trap());
+// const height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+// console.log(trap());
+
+// Brute Force Approach
+// const largestRectangleArea = function () {
+//   const n = heights.length;
+//   let area = -Infinity;
+
+//   for (let i = 0; i < n; i++) {
+//     const height = heights[i];
+//     let left = i;
+//     let right = i;
+
+//     while (left > 0 && heights[left - 1] >= heights[i]) {
+//       left--;
+//     }
+//     while (right < n - 1 && heights[right + 1] >= heights[i]) {
+//       right++;
+//     }
+
+//     const newArea = height * (right - left + 1);
+//     area = Math.max(newArea, area);
+//   }
+
+//   return area;
+// };
+
+// Optimized Approach
+
+const nextSmallerIndex = (heights, n) => {
+  const result = [];
+  const stack = new Stack();
+
+  for (let i = n - 1; i >= 0; i--) {
+    const height = heights[i];
+
+    while (!stack.isEmpty() && heights[stack.peek()] >= height) {
+      stack.pop();
+    }
+
+    if (stack.isEmpty()) {
+      result.push(-1);
+    } else {
+      result.push(stack.peek());
+    }
+    stack.push(i);
+  }
+  return result.reverse();
+};
+
+const previouSmallerIndex = (heights, n) => {
+  const result = [];
+  const stack = new Stack();
+
+  for (let i = 0; i < n; i++) {
+    const height = heights[i];
+
+    while (!stack.isEmpty() && height <= heights[stack.peek()]) {
+      stack.pop();
+    }
+
+    if (stack.isEmpty()) {
+      result.push(-1);
+    } else {
+      result.push(stack.peek());
+    }
+    stack.push(i);
+  }
+  return result;
+};
+
+const largestRectangleArea = function () {
+  let maxArea = -Infinity;
+  const n = heights.length;
+
+  const nextSmallerArray = nextSmallerIndex(heights, n);
+  const previouSmallerArray = previouSmallerIndex(heights, n);
+
+  for (let i = 0; i < n; i++) {
+    const right = nextSmallerArray[i] === -1 ? n : nextSmallerArray[i];
+    const left = previouSmallerArray[i];
+
+    const height = heights[i];
+    const area = height * (right - left - 1);
+    maxArea = Math.max(area, maxArea);
+  }
+  return maxArea;
+};
+
+const heights = [2, 1, 5, 6, 2, 3];
+console.log(largestRectangleArea());
