faf320d538c2794ac8e1f1c4d19537374a53a102
Ayush Dixit <fsayush100@gmail.com>
2025-05-04 13:43:35 +0530

    revised previous & next greater & smaller using stacks

--- stacks/chatgpt-question.js
+++ stacks/chatgpt-question.js
@@ -21,16 +21,109 @@ const isValid = function () {
           (element === "}" && peek === "{")
         ) {
           stack.pop();
-        }else{
-            return false
+        } else {
+          return false;
         }
       }
     }
   }
-  return stack.isEmpty() ? true : false
+  return stack.isEmpty() ? true : false;
 };
 
 // const s = "((";
 
 // console.log(isValid())
+const nums = [4, 8, 5, 2, 25];
+const arr = [4, 5, 2, 10, 8];
 
+// Next Greater Element
+function nextGreater() {
+  const result = [];
+  const stack = new Stack();
+  for (let i = arr.length - 1; i >= 0; i--) {
+    const element = arr[i];
+
+    while (!stack.isEmpty() && element >= stack.peek()) {
+      stack.pop();
+    }
+
+    if (stack.isEmpty()) {
+      result.push(-1);
+    } else {
+      result.push(stack.peek());
+    }
+
+    stack.push(element);
+  }
+
+  return result.reverse();
+}
+
+// console.log(nextGreater());
+
+// Previous Greater Element
+function previousGreater() {
+  const result = [];
+  const stack = new Stack();
+
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    while (!stack.isEmpty() && element >= stack.peek()) {
+      stack.pop();
+    }
+
+    if (stack.isEmpty()) {
+      result.push(-1);
+    } else {
+      result.push(stack.peek());
+    }
+    stack.push(element);
+  }
+
+  return result;
+}
+// console.log(previousGreater());
+
+// Next Smaller Element
+function nextSmaller() {
+  const result = [];
+  const stack = new Stack();
+  for (let i = nums.length - 1; i >= 0; i--) {
+    const element = nums[i];
+    while (!stack.isEmpty() && stack.peek() >= element) {
+      stack.pop();
+    }
+    if (stack.isEmpty()) {
+      result.push(-1);
+    } else {
+      result.push(stack.peek());
+    }
+    stack.push(element);
+  }
+  return result.reverse();
+}
+
+// console.log(nextSmaller());
+
+// Previous Smaller Element
+function previousSmaller() {
+  const result = [];
+  const stack = new Stack();
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+
+    while (!stack.isEmpty() && element <= stack.peek()) {
+      stack.pop();
+    }
+
+    if (stack.isEmpty()) {
+      result.push(-1);
+    } else {
+      result.push(stack.peek());
+    }
+    stack.push(element);
+  }
+  return result;
+}
+
+// console.log(previousSmaller());
