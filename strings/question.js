62de58ef2da0688fe1c6a03ef38c88905a4af423
Ayush Dixit <fsayush100@gmail.com>
2025-05-21 11:18:06 +0530

    string compression question solved

--- strings/question.js
+++ strings/question.js
@@ -31,3 +31,33 @@ const validPalindrome = function (s) {
   }
   return true;
 };
+
+// Leetcode 443. String Compression
+const compress = function (chars) {
+    if (chars.length === 1) {
+        return 1
+    }
+
+    let i = 0
+    let changeIndex = 0
+
+    while (i < chars.length) {
+        let j = i + 1
+
+        while (j < chars.length && chars[i] === chars[j]) {
+            j++
+        }
+        let count = j - i
+        chars[changeIndex++] = chars[i]
+        if (count > 1) {
+            let stringChar = String(count)
+
+            for (let m = 0; m < stringChar.length; m++) {
+                chars[changeIndex++] = stringChar[m]
+            }
+        }
+        i = j
+    }
+
+    return changeIndex
+};
