6d374ab51465563cfc5ed5a3e63e7dfb58a1ea57
Ayush Dixit <fsayush100@gmail.com>
2025-05-30 21:37:54 +0530

    leetcode question solved

--- recursion/maths.js
+++ recursion/maths.js
@@ -46,3 +46,14 @@ const gcdOfTwoNumber = (num1, num2) => {
   }
 };
 // console.log(gcdOfTwoNumber(48, 18));
+
+const isUgly = function (n) {
+    if (n <= 0) {
+        return false
+    }
+    while (n % 2 === 0) n /= 2;
+    while (n % 3 === 0) n /= 3;
+    while (n % 5 === 0) n /= 5;
+    return n === 1;
+
+};
