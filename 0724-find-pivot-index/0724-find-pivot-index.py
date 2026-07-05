e9609773983bd883603f9fb4f5ac01932a33efe9
Ayush Dixit <fsayush100@gmail.com>
2026-07-05 13:13:43 +0530

    Time: 14 ms (23.24%), Space: 20.3 MB (81.31%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -4,37 +4,22 @@ class Solution:
         if n==1:
             return 0
 
-        sum = 0
+        prefixSum = [0] * n
+        prefixSum[0] = nums[0]
+        for i in range(1,n):
+            prefixSum[i] = prefixSum[i-1] + nums[i]
 
-        for k in range(n):
-            sum += nums[k]
-        
         i = 0
         while i < n:
-            value = nums[i]
-            sum_with_out_value = sum - value
-            left_element = i - 0
-            right_element = n - 1 - i
-
-            direction = "left"
-
-            if left_element <= right_element:
-                direction = "left"
+            leftSum = 0
+            if i-1 < 0:
+                leftSum = 0
             else:
-                direction = "right"
+               leftSum = prefixSum[i-1]
 
-            sum_element = 0
-            if direction == "right":
-                for j in range(i+1, n):
-                    sum_element += nums[j]
-            else:
-                for j in range(0, i):
-                    sum_element += nums[j]
-
-            sum_value = sum_with_out_value - sum_element
-            if sum_value == sum_element:
+            rightSum = prefixSum[n-1] - prefixSum[i] 
+            if leftSum == rightSum:
                 return i
-
+            
             i+=1
-
         return -1
