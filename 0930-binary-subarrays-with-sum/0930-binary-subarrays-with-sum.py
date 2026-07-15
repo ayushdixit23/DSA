bcd0c76dc98366b5ea83173bec18a3612fd93c19
Ayush Dixit <fsayush100@gmail.com>
2026-07-15 13:43:50 +0530

    Time: 32 ms (63.57%), Space: 23.1 MB (20.94%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        n = len(nums)
+        hash_map = {}
+        hash_map[0] = 1
+        prefix_sum = 0
+        count = 0
+
+        for i in range(n):
+            prefix_sum += nums[i]
+
+            result = prefix_sum - goal
+            if result in hash_map:
+                count += hash_map[result]
+            
+            if prefix_sum in hash_map:
+                hash_map[prefix_sum] += 1
+            else:
+                hash_map[prefix_sum] = 1
+
+        return count
+                
