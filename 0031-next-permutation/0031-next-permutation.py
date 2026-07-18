905f60dbc5e7f24fe78aa1499e94b3b7e935aed3
Ayush Dixit <fsayush100@gmail.com>
2026-07-18 14:21:08 +0530

    Time: 4 ms (1.93%), Space: 19.5 MB (25.97%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,42 +1,11 @@
 class Solution:
-    def merge(self, nums, start, mid, end):
-        i = start
-        j = mid + 1
-        index = 0
-
-        temp_list = [0] * (end - start + 1)
-
-        while i <= mid and j <= end:
-            if nums[i] <= nums[j]:
-                temp_list[index] = nums[i]
-                i += 1
-                index += 1
-            else:
-                temp_list[index] = nums[j]
-                j += 1
-                index += 1
-
-        while j <= end:
-            temp_list[index] = nums[j]
-            j += 1
-            index += 1
-
-        while i <= mid:
-            temp_list[index] = nums[i]
-            i += 1
-            index += 1
-
-        for k in range(start, end + 1):
-            nums[k] = temp_list[k - start]
-
     def merge_sort(self, nums, start, end):
         if start >= end:
             return
 
         mid = (start + end) // 2
-
-        self.merge_sort(nums, start=start, end=mid)
-        self.merge_sort(nums, start=mid + 1, end=end)
+        self.merge_sort(nums, start, mid)
+        self.merge_sort(nums, mid + 1, end)
 
         if nums[mid] <= nums[mid + 1]:
             return
@@ -44,14 +13,29 @@ class Solution:
         self.merge(nums, start, mid, end)
         return
 
-    def reverse(self, nums, n):
-        i = 0
-        j = n - 1
+    def merge(self, nums, start, mid, end):
+        temp_list = []
+        i = start
+        j = mid + 1
 
-        while i < j:
-            nums[i], nums[j] = nums[j], nums[i]
+        while i <= mid and j <= end:
+            if nums[i] <= nums[j]:
+                temp_list.append(nums[i])
+                i += 1
+            else:
+                temp_list.append(nums[j])
+                j += 1
+        while i <= mid:
+            temp_list.append(nums[i])
             i += 1
-            j -= 1
+        while j <= end:
+            temp_list.append(nums[j])
+            j += 1
+
+        for index in range(start, end + 1):
+            nums[index] = temp_list[index - start]
+
+        return
 
     def nextPermutation(self, nums: List[int]) -> None:
         """
@@ -60,32 +44,36 @@ class Solution:
         n = len(nums)
         if n == 1:
             return
+
         if n == 2:
-            self.reverse(nums, n)
+            nums.reverse()
             return
 
         is_reverse_sorted = True
-        for i in range(n - 1):
-            if nums[i] < nums[i + 1]:
+
+        for i in range(1, n):
+            if nums[i] > nums[i - 1]:
                 is_reverse_sorted = False
                 break
 
         if is_reverse_sorted:
-            self.reverse(nums, n)
+            nums.reverse()
             return
 
-        j = n - 1
-        while j > 0 and nums[j - 1] >= nums[j]:
-            j -= 1
-        j -= 1
+        element = nums[n - 1]
+        index = n - 1
 
-        minimumMax = float("inf")
-        minimumIndex = -1
-        for i in range(j + 1, n):
-            if nums[i] > nums[j] and nums[i] < minimumMax:
-                minimumMax = nums[i]
+        for i in range(n - 2, -1, -1):
+            if nums[i] < element:
+                index = i
+                break
+            else:
+                element = nums[i]
+        minimumIndex = index + 1
+        for i in range(index + 1, n):
+            if nums[i] > nums[index] and nums[i] < nums[minimumIndex]:
                 minimumIndex = i
+        nums[index], nums[minimumIndex] = nums[minimumIndex], nums[index]
 
-        nums[j], nums[minimumIndex] = nums[minimumIndex], nums[j]
-
-        self.merge_sort(nums, j + 1, n - 1)
+        self.merge_sort(nums, index + 1, n - 1)
+        return
